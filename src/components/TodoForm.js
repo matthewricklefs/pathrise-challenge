135f97a1e38a365fd38b62fd03f62069284b60d6
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-03-10 19:10:31 -0500

    adds functionality to the TodoForm, TodoList and Todo components

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
   const [input, setInput] = useState("");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
   const handleChange = (e) => {
     setInput(e.target.value);
@@ -10,28 +16,27 @@ function TodoForm(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // props.onSubmit({
-    //   id: Math.floor(Math.random() * 10000),
-    //   text: input,
-    // });
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
 
     setInput("");
   };
 
   return (
-    <div>
-      <form className="todo-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Add a Todo"
-          value={input}
-          name="text"
-          className="todo-input"
-          onChange={handleChange}
-        />
-        <button className="todo-button">Add Todo</button>
-      </form>
-    </div>
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a Todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      <button className="todo-button">Add Todo</button>
+    </form>
   );
 }
 
